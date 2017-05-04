881935b29f97b45216436396d6383142d21bdba6
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-04 09:59:34 -0400

    array manipulation

---
+++ coffee-code-12.js
@@ -0,0 +1,51 @@
+// Array Manipulation
+// let is like var
+let choices = ["Blue", "Green", "Orange", "Violet", "Indigo", "Yellow"];
+let suffixes = ["th", "st", "nd", "rd"]
+
+let result = choices.map(function(choice, index) {
+  let suffix;
+  switch(index) {
+    case 0:
+      // suffix should be st
+      suffix = suffixes[1]
+      break;
+    case 1:
+      // nd
+      suffix = suffixes[2]
+      break;
+    case 2:
+      // rd
+      suffix = suffixes[3]
+      break;
+    default:
+      // th
+      suffix = suffixes[0]
+  }
+  return '${index + 1}${suffix} choice is ${choice}.'
+});
+
+
+document.body.innerHTML = result.join('<br>')
+// result is an array
+
+
+
+// for (var i = 1; i < choices.length; i++) {
+//   switch(i) {
+//     case 0:
+//       // suffix should be st
+//       break;
+//     case 1:
+//       // nd
+//       break;
+//     case 2:
+//       // th
+//       break;
+//     default:
+//   }
+// }
+
+
+
+// Current Date
